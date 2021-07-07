8f9f1b0a3b8a64de4faa1665846252ffc6761fcf
JayJayDee <jindongp@gmail.com>
2021-07-07 15:16:54 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# minecraft-info-webpage
+An informational webpage for personal minecraft server.
---
+++ src/configurator/index.js
@@ -0,0 +1,30 @@
+
+const loadMandatory = (key, env = process.env) => {
+	const value = env[key];
+	if (!value) {
+		throw new Error(`mandatory variable not supplied, key: ${key}`);
+	}
+};
+
+const configurationStore = {
+	HTTP_PORT: null,
+	MINECRAFT_REST_HOST: null
+};
+
+const initConfigurations = (store = configurationStore) => {
+	store.HTTP_PORT = loadMandatory('HTTP_PORT');
+	store.MINECRAFT_REST_HOST = loadMandatory('MINECRAFT_REST_HOST');
+};
+
+const getConfiguration = (key, store = configurationStore) => {
+	const value = store[key];
+	if (!value) {
+		throw new Error(`configuration not found for key: ${key}`);
+	}
+	return value;
+};
+
+module.exports = {
+	initConfigurations,
+	getConfiguration
+};
